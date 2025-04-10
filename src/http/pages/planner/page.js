1fe2bf7cbec3c72ef201689da1effc0d4d59cb0e
Justin <justinvdmey@hotmail.com>
2025-04-10 23:47:22 +0200

    Make good for demo

--- src/http/pages/planner/page.js
+++ src/http/pages/planner/page.js
@@ -1,6 +1,5 @@
 import '@components/accordion';
-import '../../../components/studyCard/studyCard.js';
-import '@components/studycard/studyCard.css?inline';
+import '@components/studyCard/studyCard.js';
 
 function addAccordionEventListeners() {
     const accordionItems = document.querySelectorAll('.module-item');
@@ -78,60 +77,60 @@ export default function PlannerPage(params) {
     }, 0);
 
     return /*html*/ `
-    <x-sheet id="modulesSelector" open>
-        <span style="display: flex; flex-direction: column; gap: 4px;">
-            <h5 style="margin: 0; font-size: 18px;">Modules</h5>
-            <div class="divider" style="background-color: rgb(var(--color-gray-4)); height:1px;"></div>
-            <p style="margin: 0; font-size: 12px;">
-                Dit zijn alle beschikbare modules waaruit je kunt kiezen. Als je een externe module wilt volgen, kun je deze toevoegen via de 'Anders' optie onder 'Overig'.
-            </p>
-            <custom-accordion style="--accordion-active-bg-color: #FFF4CE; --accordion-bg-color: #f1f1f1; --circle-color: rgb(var(--color-gold));">
-                <span slot="title">Software Engineering</span>
-                <div class="module-item" slot="content">
-                    <p>Webdevelopment</p>
-                </div>
-                <div class="module-item" slot="content">
-                    <p>Software Engineering</p>
-                </div>
-            </custom-accordion>
-            <custom-accordion style="--accordion-active-bg-color: rgb(var(--color-light-blue)); --accordion-bg-color: #f1f1f1; --circle-color: rgb(var(--color-sky-blue));">
-                <span slot="title">Infrastructure Design & Security</span>
-                <div class="module-item" slot="content">
-                    <p>Applied IT Security</p>
-                </div>
-                <div class="module-item" slot="content">
-                    <p>Cloud Computing</p>
-                </div>
-            </custom-accordion>
-            <custom-accordion style="--accordion-active-bg-color: rgb(var(--color-apple-green)); --accordion-bg-color: #f1f1f1; --circle-color: rgb(var(--color-dark-green));">
-                <span slot="title">Business IT & Management</span>
-                <div class="module-item" slot="content">
-                    <p>Datascience</p>
-                </div>
-                <div class="module-item" slot="content">
-                    <p>Management of IT</p>
-                </div>
-            </custom-accordion>
-            <custom-accordion style="--accordion-active-bg-color: #FEDFE4; --accordion-bg-color: #f1f1f1; --circle-color: rgb(var(--color-dark-pink));">
-                <span slot="title">Overig</span>
-                <div class="module-item" slot="content">
-                    <p>Tussen jaar</p>
-                </div>
-                <div class="module-item" slot="content">
-                    <p>Minor</p>
-                </div>
-                <div class="module-item" slot="content">
-                    <p>Eigen Keuze</p>
-                </div>
-            </custom-accordion>
-        </span>
-    </x-sheet>
+        <x-sheet id="modulesSelector" open>
+            <span style="display: flex; flex-direction: column; gap: 4px;">
+                <h5 style="margin: 0; font-size: 18px;">Modules</h5>
+                <div class="divider" style="background-color: rgb(var(--color-gray-4)); height:1px;"></div>
+                <p style="margin: 0; font-size: 12px;">
+                    Dit zijn alle beschikbare modules waaruit je kunt kiezen. Als je een externe module wilt volgen, kun je deze toevoegen via de 'Anders' optie onder 'Overig'.
+                </p>
+                <custom-accordion style="--accordion-active-bg-color: #FFF4CE; --accordion-bg-color: #f1f1f1; --circle-color: rgb(var(--color-gold));">
+                    <span slot="title">Software Engineering</span>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Webdevelopment</p>
+                    </div>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Software Engineering</p>
+                    </div>
+                </custom-accordion>
+                <custom-accordion style="--accordion-active-bg-color: rgb(var(--color-light-blue)); --accordion-bg-color: #f1f1f1; --circle-color: rgb(var(--color-sky-blue));">
+                    <span slot="title">Infrastructure Design & Security</span>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Applied IT Security</p>
+                    </div>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Cloud Computing</p>
+                    </div>
+                </custom-accordion>
+                <custom-accordion style="--accordion-active-bg-color: rgb(var(--color-apple-green)); --accordion-bg-color: #f1f1f1; --circle-color: rgb(var(--color-dark-green));">
+                    <span slot="title">Business IT & Management</span>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Datascience</p>
+                    </div>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Management of IT</p>
+                    </div>
+                </custom-accordion>
+                <custom-accordion style="--accordion-active-bg-color: #FEDFE4; --accordion-bg-color: #f1f1f1; --circle-color: rgb(var(--color-dark-pink));">
+                    <span slot="title">Overig</span>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Tussen jaar</p>
+                    </div>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Minor</p>
+                    </div>
+                    <div class="module-item" slot="content">
+                        <p style="padding-top: 12px; padding-bottom: 12px; margin-left: 10px;">Eigen Keuze</p>
+                    </div>
+                </custom-accordion>
+            </span>
+        </x-sheet>
 
-    <div class="study-cards-container" style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 20px; justify-content: center; padding: 20px;">
-        ${addStudyCard('Jaar 1', 'Semester 1', 'Basisconcepten ICT 1', 'Semester 2', 'Basisconcepten ICT 2')}
-        ${addStudyCard('Jaar 2', 'Semester 1', 'Keuze 1', 'Semester 2', 'Keuze 2')}
-        ${addStudyCard('Jaar 3', 'Semester 1', 'Keuze 1', 'Semester 2', 'Keuze 2')}
-        ${addStudyCard('Jaar 4', 'Semester 1', 'Keuze 1', 'Semester 2', 'Keuze 2')}
-    </div>
+        <div class="study-cards-container" style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 20px; justify-content: center; padding: 20px; margin:auto;">
+            ${addStudyCard('Jaar 1', 'Semester 1', 'Basisconcepten ICT 1', 'Semester 2', 'Basisconcepten ICT 2')}
+            ${addStudyCard('Jaar 2', 'Semester 1', 'Keuze 1', 'Semester 2', 'Keuze 2')}
+            ${addStudyCard('Jaar 3', 'Semester 1', 'Keuze 1', 'Semester 2', 'Keuze 2')}
+            ${addStudyCard('Jaar 4', 'Semester 1', 'Keuze 1', 'Semester 2', 'Keuze 2')}
+        </div>
     `;
 }
