6e10cd80e2c40f0b08fe1f33f53e1771a4e287a2
Justin <justinvdmey@hotmail.com>
2025-04-12 15:23:47 +0200

    Fix

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ export const routes = [
         .add('/', () => import('@/http/pages/page.js'))
         .add('/:uuid', () => import('@/http/pages/page.js')).routes,
 
-    ...new RouteGroup([new RequireAuthCookie(), new EnsureCohortIsSet()])
+    ...new RouteGroup([new RequireAuthCookie()])
         .add('/messages', () => import('@pages/messages/page.js'))
         .add('/admin/modules', () => import('@pages/modules/page.js'))
         .add('/admin/modules/:uuid', () => import('@pages/modules/page.js')).routes,
